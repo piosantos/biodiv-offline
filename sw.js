e31308c334519096d18e35527d3ad7fa8fc36c5a
piosantos <pio.moneymaker@gmail.com>
2025-09-12 17:50:41 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,44 @@
-self.addEventListener('install', e=>{self.skipWaiting()});
+// sw.js (root paths, cache bump)
+const CACHE = 'biodiversity-offline-v4';
+const CORE = [
+  './',
+  './index.html',
+  './tf.min.js',
+  './mobilenet.min.js',
+  './chart.umd.min.js',
+  './html2canvas.min.js',
+  './jspdf.umd.min.js'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE)
+      .then(c => c.addAll(CORE.filter(Boolean)))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  const req = e.request;
+  e.respondWith(
+    caches.match(req).then(cached =>
+      cached ||
+      fetch(req).then(res => {
+        if (req.url.startsWith(self.location.origin)) {
+          const clone = res.clone();
+          caches.open(CACHE).then(c => c.put(req, clone));
+        }
+        return res;
+      }).catch(() => cached)
+    )
+  );
+});
